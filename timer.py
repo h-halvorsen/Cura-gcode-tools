ef04888db3e6c624631c68e2d15e8b271fa7df2f
henrik2900 <henrikaaslie@hotmail.com>
2021-03-28 12:39:32 +0200

    Added file

---
+++ timer.py
@@ -0,0 +1,55 @@
+import sys
+
+word = ';TIME_ELAPSED:'
+
+s_total = 0.0
+
+def sec_to_hhmmss(val):
+	hh = val/3600 - ((val/3600)%1)
+	mm = (val/60)-((val/60)%1) - (hh*60)
+	ss = val - mm*60 - hh * 3600
+
+	hh = int(hh)
+	mm = int(mm)
+	ss = int(ss)
+
+	out = "{}h {}m {}s".format(hh,mm,ss)
+
+	return out
+
+
+def findTime(path):
+	global s_total
+	
+	if path[0] == '\"' or path[0] == '\'':
+		path = path[1:-1]
+	f_name = path[::-1]
+	idx = f_name.index("\\")
+	f_name = f_name[:idx]
+	f_name = f_name[::-1]
+	print(f_name)
+
+	with open(path) as file:
+		for line in reversed(file.readlines()):
+			if word in line:
+				idx = line.index(':')
+				substr = line[idx+1:-1]
+
+				val = float(substr)
+				s_total += val
+				val = int(val)
+
+				print(sec_to_hhmmss(val))
+				break
+
+if len(sys.argv) < 2:
+	findTime(input("File path: "))
+else:
+	for i in range(1, len(sys.argv)):
+		findTime(sys.argv[i])
+		print()
+
+total = sec_to_hhmmss(s_total)
+print("Total: {}".format(total))
+
+input()
