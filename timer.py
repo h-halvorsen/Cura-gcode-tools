d8849450355581a33861f9bd5e5bcf9aabc7044d
henrik halvorsen <henrikaaslie@hotmail.com>
2022-03-26 05:21:17 +0100

    added modified path check

--- timer.py
+++ timer.py
@@ -54,7 +54,6 @@ if __name__ == "__main__":
 		for i in range(1, len(sys.argv)):
 			path = sys.argv[i]
 			filenameFromPath(path)
-
 			out, length = findTime(path)
 			print(out)
 			print()			#separator line
